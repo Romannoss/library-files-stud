97aa74556801331de01037adc71fa51ef81ea48e
Giovanni Bisio <43917212+Romannoss@users.noreply.github.com>
2021-03-30 19:26:10 -0300

    update 30/03/21
    update 30/03/21

---
+++ script-js-frame.js
@@ -0,0 +1,26 @@
+const modal(nome do objeto) = {
+
+    open() {
+
+        //abrir modal
+        // add a class ao modal
+
+        document
+            .querySelector('[nome da classe para ativar]')
+        [nome da class]
+            .add('active')
+}
+
+close(){
+
+    //fechar o modal
+    //remover a class active do modal
+
+    document
+        .querySelector('[nome da classe para ativar]')
+    [nome da class]
+            .remove('active')
+
+}
+
+}
